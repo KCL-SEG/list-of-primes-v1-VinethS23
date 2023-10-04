02930de8f5971a363fda6f3a8a4de39708bdfa35
Vineth Siriwardana <VASiriwardana@gmail.com>
2023-10-04 14:25:55 +0100

    Finished + tested the list of primes

--- primes.py
+++ primes.py
@@ -3,4 +3,16 @@
 
 def primes(number_of_primes):
     list = []
+    counter = 2
+    while len(list) < number_of_primes:
+        divisible_numbers = 0
+        for i in range(counter):
+            if counter % (i+1) == 0:
+                divisible_numbers += 1
+        if divisible_numbers == 2:
+            list.append(counter)
+        counter += 1
+    print(list)
     return list
+
+primes(20)
